ee4afd05ac74c2a4dbf351bfdc337e9e434b4285
Gabriel Vitor <git624886@gmail.com>
2023-10-25 09:01:56 -0300

    feat: pgsql database

--- config/database.php
+++ config/database.php
@@ -76,16 +76,13 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'search_path' => 'public',
-            'sslmode' => 'prefer',
+            'host' => env('DB_HOST'),
+            'port' => env('DB_PORT'),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+            // 'search_path' => 'public',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
