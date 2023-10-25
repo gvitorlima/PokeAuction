6e9cbc8f12f6d48a01f9c0bbda562a830d9326e1
Gabriel Vitor <git624886@gmail.com>
2023-10-25 11:31:04 -0300

    feat: migrate models

---
+++ database/migrations/2023_10_24_094314_create_pokemon_status_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pokemon_status', function (Blueprint $blueprint) {
+
+            $blueprint->foreignId('pokemon_id')->constrained('pokemon');
+            $blueprint->foreignId('status_id')->constrained('status');
+
+            $blueprint->integer('value', 6);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pokemon_status');
+    }
+};
