f39337356bda86112a2d413c7fb191ed6b72e403
Gabriel Vitor <git624886@gmail.com>
2023-10-26 07:49:09 -0300

    feat: migrate pokemon ability

---
+++ database/migrations/2023_10_26_072939_pokemon_ability.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+
+        Schema::create('pokemon_ability', function (Blueprint $blueprint) {
+            $blueprint->increments('id');
+
+            $blueprint->foreignId('pokemon_id')->constrained('pokemon', 'pokemon_id');
+            $blueprint->foreignId('ability_id')->constrained('ability', 'id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pokemon_ability');
+    }
+};
